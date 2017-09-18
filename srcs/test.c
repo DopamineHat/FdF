aeeb69de6fcb08e41471a2556a69d1808f8b3e86
UglyHat <rpagot@hotmail.fr>
2017-09-18 07:43:20 +0200

    YEZZZZ

--- srcs/test.c
+++ srcs/test.c
@@ -6,7 +6,7 @@
 /*   By: rpagot <rpagot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/12 16:08:32 by rpagot            #+#    #+#             */
-/*   Updated: 2017/09/18 07:29:33 by rpagot           ###   ########.fr       */
+/*   Updated: 2017/09/18 06:30:20 by rpagot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,30 @@ void				ft_map_display(t_map *map)
 	int	x2;
 	int	y1;
 	int	y2;
+	float	m;
 	int	mx;
 	int	my;
-	int	signx;
-	int	signy;
 	int	d;
+	int	x;
+	int x3;
+	int y3;
+	int x4;
+	int y4;
 	int d2;
+	int mx2;
+	int my2;
+	int xx;
 	int y;
 
-	x1 = 400;
-	y1 = 400;
+	x1 = 10;
+	y1 = 10;
+	x3 = 350;
+	y3 = 50;
 
-	x2 = 350;
-	y2 = 50;
+	x2 = 400;
+	y2 = 250;
+	x4 = 50;
+	y4 = 250;
 	if (!(map->win = mlx_new_window(map->mlx, 500, 500, "yolo")))
 		return ;
 	if (!(map->image = mlx_new_image(map->mlx, 500, 500)))
@@ -42,27 +53,41 @@ void				ft_map_display(t_map *map)
 	int	*addr;
 
 	addr = (int*) map->addr;
-	mx = abs(x2 - x1);
-	my = -abs(y2 - y1);
-	signx = x1 < x2 ? 1 : -1;
-	signy = y1 < y2 ? 1 : -1;
-	d = mx + my;
-	while (x1 + y1 * 500 < 500 * 500)
+	m = (x2 - x1) / (y2 - y1);
+	mx = x2 - x1;
+	my = y2 - y1;
+	mx2 = x4 - x3;
+	my2 = y4 - y3;
+	x = x1 + y1 * 500;
+	xx = x3 + y3 * 500;
+	d = 2 * my - mx;
+	d2 = 2 * my2 - mx2;
+	y = y3 * 2;
+	while (x < 500 * 500 && xx < 500 * 500)
 	{
-		addr[x1 + y1 * 500] = 0x00FFFFFF;
-		if (x1 + y1 * 500 == x2 + y2 * 500)
-			break;
-		d2 = d * 2;
-		if (d2 >= my)
+		if (x > x1 + y1 * 500 && x <= y2 * 500 + x2)
 		{
-			d += my;
-			x1 += signx;
+			addr[x] = 0x00FFFFFF;
+			if (d > 0)
+			{
+				x += 500;
+				d -= 2 * mx;
+			}
+			d += 2 * my;
 		}
-		if (d2 <= mx)
+		if (xx > x3 + y3 * 500 && xx <= y4 * 500 + x4)
 		{
-			d += mx;
-			y1 += signy;
+			addr[- xx + y * 500 + 350 + 350] = 0x00FFFFFF;
+			if (d2 > 0)
+			{
+				xx += 500;
+				y += 2;
+				d2 -= 2 * mx2;
+			}
+			d2 += 2 * my2;
 		}
+		x++;
+		xx++;
 	}
 	mlx_put_image_to_window(map->mlx, map->win, map->image, 0, 0);
 	mlx_loop(map->mlx);
